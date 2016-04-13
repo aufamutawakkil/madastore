6bda9f1aaee567805053c4ce61e99e361e3e705c
aufa mutawakkil <aufamutawakkil@gmail.com>
2016-04-13 11:06:32 +0700

    fixing project name config

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/tooko/admin/');
-define('HTTP_CATALOG', 'http://localhost/tooko/');
+define('HTTP_SERVER', 'http://localhost/madastore/admin/');
+define('HTTP_CATALOG', 'http://localhost/madastore/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/tooko/admin/');
-define('HTTPS_CATALOG', 'http://localhost/tooko/');
+define('HTTPS_SERVER', 'http://localhost/madastore/admin/');
+define('HTTPS_CATALOG', 'http://localhost/madastore/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/tooko/admin/');
-define('DIR_SYSTEM', '/var/www/html/tooko/system/');
-define('DIR_LANGUAGE', '/var/www/html/tooko/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/tooko/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/tooko/system/config/');
-define('DIR_IMAGE', '/var/www/html/tooko/image/');
-define('DIR_CACHE', '/var/www/html/tooko/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/tooko/system/storage/download/');
-define('DIR_LOGS', '/var/www/html/tooko/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/html/tooko/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/html/tooko/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/html/tooko/catalog/');
+define('DIR_APPLICATION', '/var/www/html/madastore/admin/');
+define('DIR_SYSTEM', '/var/www/html/madastore/system/');
+define('DIR_LANGUAGE', '/var/www/html/madastore/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/madastore/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/madastore/system/config/');
+define('DIR_IMAGE', '/var/www/html/madastore/image/');
+define('DIR_CACHE', '/var/www/html/madastore/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/madastore/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/madastore/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/madastore/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/madastore/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/madastore/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/tooko/');
+define('HTTP_SERVER', 'http://localhost/madastore/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/tooko/');
+define('HTTPS_SERVER', 'http://localhost/madastore/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/tooko/catalog/');
-define('DIR_SYSTEM', '/var/www/html/tooko/system/');
-define('DIR_LANGUAGE', '/var/www/html/tooko/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/html/tooko/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/html/tooko/system/config/');
-define('DIR_IMAGE', '/var/www/html/tooko/image/');
-define('DIR_CACHE', '/var/www/html/tooko/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/tooko/system/storage/download/');
-define('DIR_LOGS', '/var/www/html/tooko/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/html/tooko/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/html/tooko/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/html/madastore/catalog/');
+define('DIR_SYSTEM', '/var/www/html/madastore/system/');
+define('DIR_LANGUAGE', '/var/www/html/madastore/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/madastore/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/madastore/system/config/');
+define('DIR_IMAGE', '/var/www/html/madastore/image/');
+define('DIR_CACHE', '/var/www/html/madastore/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/madastore/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/madastore/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/madastore/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/madastore/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
